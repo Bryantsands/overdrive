777df5c72bf886bd12d90bdf435e13c43dddfe21
bryantsands <bryantsands@bryantsands.attlocal.net>
2017-08-28 19:54:02 -0500

    added routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -16,12 +16,34 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
   bd.product.create({
     product_name: req.body.product_name,
+    bought: false
   }).then(function(dbProduct){
     console.log(dbProduct);
     res.redirect('/');
   });catch(function(error){
-    thrw error;
+    throw error;
   });
 });
 
-router.put('/:id', )
+router.put('/:id', function(req, res){
+  db.product.update({
+    bought: req.body.bought,
+    customer: req.body.customer
+  }, {
+    where: {id: req.params.id}
+  }).then(function(){
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res){
+  db.product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(){
+    res.redirect("/");
+  });
+});
+
+};
